c31c80d89867f64d41d88e11d9ef5dd2ee7b490b
Hardware-Man <kmishra686@gmail.com>
2020-10-01 13:12:19 -0400

    Update

--- src/MoneyMarket.java
+++ src/MoneyMarket.java
@@ -6,10 +6,12 @@
 public class MoneyMarket extends Account{
     private int withdrawals;
 
+    @Override
     public double monthlyInterest() {
         return 0;
     }
 
+    @Override
     public double monthlyFee() {
         return 0;
     }
--- src/Savings.java
+++ src/Savings.java
@@ -6,10 +6,12 @@
 public class Savings extends Account{
     private boolean isLoyal;
 
+    @Override
     public double monthlyInterest() {
         return 0;
     }
 
+    @Override
     public double monthlyFee() {
         return 0;
     }
